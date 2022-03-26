fd79e922f7a9ea2075e59c17d3d981f7eff9889a
veeraviswesh <veeraviswesh@gmail.com>
2022-03-26 17:46:46 +0530

    git install

---
+++ LanguageClass.java
@@ -0,0 +1,17 @@
+package org.all;
+import org.telugu.*;
+
+public class LanguageClass extends Telugu {
+	public void alllanguage() {
+		System.out.println("I know all languages");
+	}
+	public static void main(String[] args) {
+		LanguageClass all = new LanguageClass();
+		all.alllanguage();
+		all.englishlanguage();
+		all.tamillanguage();
+		all.telugulanguage();
+		
+	}
+
+}
